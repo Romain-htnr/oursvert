dc1e674983d077c9136737916e96d74447da23e7
Romain Hethener <romain.hethener@gmail.com>
2020-03-14 10:09:26 +0100

    5pages routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'lemeuble', to: 'pages#lemeuble'
+  get 'nature', to: 'pages#nature'
+  get 'restauration', to: 'pages#restauration'
+  get 'cultureetpatrimoine', to: 'pages#cultureetpatrimoine'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
