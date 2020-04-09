26bffe9966c804eb7338be28e5a1035ad9b3f468
Romain Hethener <romain.hethener@gmail.com>
2020-04-09 18:13:41 +0200

    ok

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require_relative 'boot'
 
 require 'pages_controller/railtie'
 require 'sprockets/railtie'
-require 'rails/test_unit/railtie'
+# require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
