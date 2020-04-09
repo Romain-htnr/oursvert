096a9c4199dff0512a347f6eb7993b6b0241d9fe
Romain Hethener <romain.hethener@gmail.com>
2020-04-09 18:30:16 +0200

    ok

--- config/application.rb
+++ config/application.rb
@@ -1,21 +1,7 @@
 require_relative 'boot'
 
-# require 'rails/all'
-require "active_model/railtie"
+require 'rails/all'
 
-# require "active_record/railtie"
-
-# require "action_controller/railtie"
-require "pages_controller/railtie"
-# require "action_mailer/railtie"
-
-# require "action_view/railtie"
-
-require "sprockets/railtie"
-
-require "rails/test_unit/railtie"
-
-# instead of require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
