6b4e15aee03e441c2fa6bb5e46f6f6251def5482
Romain Hethener <romain.hethener@gmail.com>
2020-04-10 10:11:03 +0200

    cable engine

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 # require 'rails/all'
 require "action_controller/railtie"
+require "action_cable/engine"
 require "action_mailer/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
